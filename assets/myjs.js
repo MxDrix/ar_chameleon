3480656fe197d10f910c7a816d5c540204c1c099
chandon <richard.chandon@gmail.com>
2018-10-26 03:02:10 +0200

    Update myjs.js

--- assets/myjs.js
+++ assets/myjs.js
@@ -1,6 +1,4 @@
 window.addEventListener("resize", function(){
-  var video = document.getElementById('video');
-  var canvas = document.getElementById('canvas');
   video.width = window.innerWidth;
   video.height = window.innerHeight;
   canvas.width = window.innerWidth;
@@ -8,69 +6,38 @@ window.addEventListener("resize", function(){
 });
 
 window.onload = function() {
-  var video = document.getElementById('video');
-  var canvas = document.getElementById('canvas');
+  const video = document.getElementById('video');
+  const canvas = document.getElementById('canvas');
+  let colorSample = ['#003366' , '#00ffff' , '#000066' , '#6699ff' , '#660066' , '#ff66ff' , '#993333' , '#ff6666' , '#663300' , '#ffff66' , '#003300' , '#66ff66' , '#ffffff' , '#000000'];  
   video.width = window.innerWidth;
   video.height = window.innerHeight;
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   var context = canvas.getContext('2d');
-
-  /* 
-  Pour ajouter manuellement de nouvelle couleur, il faut utiliser la fonction juste en dessous, en lui indiquant le rgb de la couleur
-  C'est assez approximatif, comme tu peux le voir dans le if, pour le black il faut que le tracking détecte que les couleurs à l'écran soit inférieur à 50 pour le r, g et b.
-  Faudrait faire une petite fonction qui récupère toutes les couleurs existante, bien prise de tête ça ^^
-  Il faudrait créer un tableau comme ceci : 
-  ["nomcouleur" => "rgb","nomcouleur2" => "rgb2", etc]
-  */
-  tracking.ColorTracker.registerColor('#000000', function(r, g, b) {
-    if (r < 50 && g < 50 && b < 50) {
-      return true;
-    }
-    return false;
-  });
-  tracking.ColorTracker.registerColor('#ffffff', function(r, g, b) {
-    if (r > 200 && g > 200 && b > 200) {
-      return true;
-    }
-    return false;
-  });
-
-
-
-  /*
-  Une fois les nouvelles couleurs enregistrer, on peux les initialiser dans notre nouveau tracker
-  c'est un tableau de couleur.
-  */
-  /*  var tracker = new tracking.ColorTracker(['magenta', 'cyan', 'yellow', 'black','white']);
-  */  
   var tracker = new tracking.ColorTracker(addingColors(colorSample));
   
   tracking.track('#video', tracker, { camera: true });
   tracking.ColorTracker.prototype.minDimension = 10;
   tracking.ColorTracker.prototype.minGroupSize = 10;
+  console.log(context);
   tracker.on('track', function(event) {
     const allColors = new Array();
     const element = document.getElementById('chameleon');
     context.clearRect(0, 0, canvas.width, canvas.height);
-
     event.data.forEach(function(rect) {
-      console.log(rect.color);
+      //console.log(rect.color);
       if (rect.color === 'custom') {
         rect.color = tracker.customColor;
       }
-      context.strokeStyle = rect.color;
-      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-      context.font = '11px Helvetica';
-      context.fillStyle = "#fff";
-      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-      // Ajout de la couleur sur l'élément afficher à l'écran, il va falloir faire ne sorte de bien récupérer la moyenne de toutes les couleurs pour lui ajout en background ou autre
+      // context.strokeStyle = rect.color;
+      // context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      // context.font = '11px Helvetica';
+      // context.fillStyle = "#fff";
+      // context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+      // context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22); 
       allColors.push(rect.color);
-/*      console.log(allColors);
-*/
-      mixColor(allColors);
     });
+    mixColor(allColors);
   });
 };
 
@@ -114,15 +81,15 @@ function synthesize(tab) {
   lightColorB = mainColorB + 75;
 
   colorsMixed.push(mainColorR , mainColorG , mainColorB);
-  colorsApply();
-}
-
-function colorsApply() {
+  
   mainColorFill = 'rgb(' + mainColorR + ',' + mainColorG + ',' + mainColorB + ')';
   darkColorFill = 'rgb(' + darkColorR + ',' + darkColorG + ',' + darkColorB + ')';
   strongDarkColorFill = 'rgb(' + strongDarkColorR + ',' + strongDarkColorG + ',' + strongDarkColorB + ')';
   lightColorFill = 'rgb(' + lightColorR + ',' + lightColorG + ',' + lightColorB + ')';
+  colorsApply(mainColorFill,darkColorFill,strongDarkColorFill,lightColorFill);
+}
 
+function colorsApply(mainColorFill,darkColorFill,strongDarkColorFill,lightColorFill) {
   var base = document.getElementById('base');
   base.style.fill = mainColorFill;
 
@@ -184,11 +151,3 @@ function addingColors(tab) {
   }
   return watchedColors;
 }
-
-
-let colorSample = ['#003366' , '#00ffff' , '#000066' , '#6699ff' , '#660066' , '#ff66ff' , '#993333' , '#ff6666' , '#663300' , '#ffff66' , '#003300' , '#66ff66' , '#ffffff' , '#000000'];
-
-/*document.addEventListener("DOMContentLoaded", function() {
-  mixColor(colorSample);  
-})
-*/
